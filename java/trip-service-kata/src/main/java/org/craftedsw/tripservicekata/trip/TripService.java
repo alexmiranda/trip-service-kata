2e7e65b79e99c9afc3672d0fbe9eec2d273056bd
alexmiranda <alexmirandamoraes@gmail.com>
2013-06-17 23:44:56 -0300

    Revert "adds java doc" This reverts commit 0d5c9734ee94ba4bbf85980ee2ef1fe6a6321a70.

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -11,7 +11,7 @@ public class TripService {
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
 		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = getLoggedInUser();
+		User loggedUser = UserSession.getInstance().getLoggedUser();
 		boolean isFriend = false;
 		if (loggedUser != null) {
 			for (User friend : user.getFriends()) {
@@ -27,10 +27,6 @@ public class TripService {
 		} else {
 			throw new UserNotLoggedInException();
 		}
-	}
-
-	protected User getLoggedInUser() {
-		return UserSession.getInstance().getLoggedUser();
 	}
 	
 }
