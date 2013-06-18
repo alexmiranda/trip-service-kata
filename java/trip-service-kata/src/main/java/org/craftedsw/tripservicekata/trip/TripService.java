ba9757377b7d080c2a1c5f775caf01a7ac4d0bb0
alexmiranda <alexmirandamoraes@gmail.com>
2013-06-17 23:44:19 -0300

    Revert "adds line comments into TripService" This reverts commit c2161d24264edb97e30d4520635a64aa1edeb7e1.

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -9,43 +9,28 @@ import org.craftedsw.tripservicekata.user.UserSession;
 
 public class TripService {
 
-	/**
-	 * Get trips by user
-	 * @param user 
-	 * @return trips by user
-	 * @throws UserNotLoggedInException when no user is logged in
-	 */
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		// trip list
 		List<Trip> tripList = new ArrayList<Trip>();
-		
-		// gets logged in user
-		User loggedUser = UserSession.getInstance().getLoggedUser();
-		
-		// is friend
+		User loggedUser = getLoggedInUser();
 		boolean isFriend = false;
-		
-		// check whether user is logged in
 		if (loggedUser != null) {
-			// if so, checks whether users are friends
 			for (User friend : user.getFriends()) {
 				if (friend.equals(loggedUser)) {
 					isFriend = true;
 					break;
 				}
 			}
-			
-			// if they are, return user trips
 			if (isFriend) {
-				// get trips from TripDAO
 				tripList = TripDAO.findTripsByUser(user);
 			}
-			
-			// return trips
 			return tripList;
 		} else {
-			// throw exception if user is not logged in
 			throw new UserNotLoggedInException();
 		}
-	}	
+	}
+
+	protected User getLoggedInUser() {
+		return UserSession.getInstance().getLoggedUser();
+	}
+	
 }
