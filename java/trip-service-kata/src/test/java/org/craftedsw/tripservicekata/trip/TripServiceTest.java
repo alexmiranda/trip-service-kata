0146f568ed24d66cc9fab7b2a4e207699fa153be
alexmiranda <alexmirandamoraes@gmail.com>
2013-06-17 23:58:49 -0300

    test code coverage

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,71 @@
 package org.craftedsw.tripservicekata.trip;
 
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+import java.util.List;
+
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.Before;
+import org.junit.Test;
+
 public class TripServiceTest {
+	
+	private static final User GUEST = null;
+	private static final User UNUSED_USER = null;
+	private static final User REGISTRED_USER = new User();
+	private static final User A_USER = new User();
+	private static final User SOMEONE_ELSE = new User();
+	
+	private static final Trip TO_BELO_HORIZONTE = new Trip();
+	private static final Trip TO_FLORIPA = new Trip();
+	
+	public User loggedInUser;
+	private TestableTripService tripService;
+	
+	@Before
+	public void initialise() {
+		tripService = new TestableTripService();
+	}
+
+	@Test(expected = UserNotLoggedInException.class)
+	public void should_throw_exception_when_user_is_not_logged_in() throws Exception {
+		loggedInUser = GUEST;
+		tripService.getTripsByUser(UNUSED_USER);
+	}
+	
+	@Test
+	public void should_not_return_any_trips_when_users_are_not_friends() throws Exception {
+		loggedInUser = REGISTRED_USER;
+		final List<Trip> friendTrips = tripService.getTripsByUser(A_USER);
+		assertThat(friendTrips.size(), is(0));
+	}
+	
+	@Test
+	public void should_return_friend_trips_when_users_are_friends() throws Exception {
+		loggedInUser = REGISTRED_USER;
+		
+		User user = new User();
+		user.addFriend(loggedInUser);
+		user.addFriend(SOMEONE_ELSE);
+		user.addTrip(TO_BELO_HORIZONTE);
+		user.addTrip(TO_FLORIPA);
+		
+		final List<Trip> friendTrips = tripService.getTripsByUser(user);
+		assertThat(friendTrips.size(), is(2));
+	}
+	
+	private class TestableTripService extends TripService {
+		@Override
+		protected User getLoggedInUser() {
+			return loggedInUser;
+		}
+		
+		@Override
+		protected List<Trip> tripsBy(User user) {
+			return user.trips();
+		}
+	}
 	
 }
